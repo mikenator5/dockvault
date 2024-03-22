62d142e1bc8a1d83d994c5bebf821ffa14381bfd
Mike Dunn <mikenator5@gmail.com>
2024-03-22 14:45:28 -0600

    Add config cli

--- helpers/config.go
+++ helpers/config.go
@@ -8,63 +8,91 @@ import (
 )
 
 type Config struct {
-	AWS   aws   `json:"aws"`
-	Azure azure `json:"azure"`
+	AWS   *AWS   `json:"aws"`
+	Azure *Azure `json:"azure"`
 }
 
-type aws struct {
+type AWS struct {
 	Bucket string `json:"bucket"`
 	Region string `json:"region"`
 }
 
-type azure struct {
+type Azure struct {
 	Account   string `json:"account"`
 	Container string `json:"container"`
 }
 
-func NewConfig() (Config, error) {
-	filePath := filepath.Join(os.Getenv("HOME"), ".dockvault", "config")
-
-	c := Config{}
-	_, err := os.Stat(filePath)
-	if err == nil {
-		// File exists
-		cData, err := os.ReadFile(filePath)
-		if err != nil {
-			return Config{}, err
-		}
-
-		if err = json.Unmarshal(cData, &c); err != nil {
-			return Config{}, err
-		}
-
-	} else if os.IsNotExist(err) {
-		// Create directory if it doesn't exist
-		dir := filepath.Dir(filePath)
-		fmt.Println(dir)
-		if err := os.MkdirAll(dir, 0755); err != nil {
-			return Config{}, err
-		}
-
-		// Create config file
-		file, err := os.Create(filePath)
-		if err != nil {
-			return Config{}, err
-		}
-
-		// Write basic data to config file
-		cData, err := json.Marshal(c)
-		if err != nil {
-			return Config{}, err
-		}
-
-		file.Write(cData)
-		defer file.Close()
-
-	} else {
-		// Some other error
-		return Config{}, err
+func writeConfig(cfg Config, filePath string) error {
+	// Create directory if it doesn't exist
+	dir := filepath.Dir(filePath)
+	fmt.Println(dir)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
 	}
 
-	return c, nil
+	// Create config file
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Write basic data to config file
+	cData, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	if _, err = file.Write(cData); err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return nil
+}
+
+func updateConfig(cfg Config) error {
+	filePath := filepath.Join(os.Getenv("HOME"), ".dockvault", "config.json")
+
+	_, err := os.Stat(filePath)
+	if err == nil {
+		// File exists => update it
+		cData, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
+
+		c := Config{}
+		if err = json.Unmarshal(cData, &c); err != nil {
+			return err
+		}
+
+		if cfg.AWS != nil {
+			c.AWS = cfg.AWS
+		}
+		if cfg.Azure != nil {
+			c.Azure = cfg.Azure
+		}
+
+		return writeConfig(c, filePath)
+
+	} else if os.IsNotExist(err) {
+		// File does not exist => create it
+		return writeConfig(cfg, filePath)
+	}
+
+	return nil
+}
+
+func NewAWSConfig(aws *AWS) error {
+	cfg := Config{
+		AWS: aws,
+	}
+	return updateConfig(cfg)
+}
+
+func NewAzureConfig(az *Azure) error {
+	cfg := Config{
+		Azure: az,
+	}
+	return updateConfig(cfg)
 }
--- main.go
+++ main.go
@@ -2,49 +2,95 @@ package main
 
 import (
 	"dockVault/helpers"
-	"dockVault/storage"
-	"log"
+	"dockVault/internal/pkg/output"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-	c, err := helpers.NewConfig()
-	if err != nil {
-		log.Fatalln("Config failed", err)
-	}
-	d, err := helpers.NewDocker()
-	if err != nil {
-		log.Fatal("Failed to start Docker... is it running?")
+
+	args := os.Args
+	if len(args) <= 1 {
+		output.PrintUsage()
+		return
 	}
 
-	// s3, err := storage.NewS3(c, d)
+	switch args[1] {
+	case "configure":
+		if len(args) <= 2 {
+			fmt.Println("usage: dockvault configure <aws|azure>")
+			return
+		}
+		awsCmd := flag.NewFlagSet("aws", flag.ExitOnError)
+		var awsBucket string
+		awsCmd.StringVar(&awsBucket, "bucket", "", "name of the bucket")
+		awsCmd.StringVar(&awsBucket, "b", "", "name of the bucket")
+		var awsRegion string
+		awsCmd.StringVar(&awsRegion, "region", "", "aws region")
+		awsCmd.StringVar(&awsRegion, "r", "", "aws region")
+
+		// az -a,--account -c,--container
+		azureCmd := flag.NewFlagSet("az", flag.ExitOnError)
+		var azureAccount string
+		azureCmd.StringVar(&azureAccount, "a", "", "azure account")
+		azureCmd.StringVar(&azureAccount, "account", "", "azure account")
+		var azureContainer string
+		azureCmd.StringVar(&azureContainer, "c", "", "azure blob container")
+		azureCmd.StringVar(&azureContainer, "container", "", "azure blob container")
+
+		switch args[2] {
+		case "aws":
+			awsCmd.Parse(args[3:])
+			if len(awsBucket) < 1 && len(awsRegion) < 1 {
+				awsCmd.Usage()
+				return
+			}
+
+			aws := helpers.AWS{
+				Bucket: awsBucket,
+				Region: awsRegion,
+			}
+
+			err := helpers.NewAWSConfig(&aws)
+			if err != nil {
+				fmt.Println("Error creating aws config file")
+				return
+			}
+		case "az":
+			azureCmd.Parse(args[3:])
+			if len(azureAccount) < 1 && len(azureContainer) < 1 {
+				azureCmd.Usage()
+				return
+			}
+
+			az := helpers.Azure{
+				Account:   azureAccount,
+				Container: azureContainer,
+			}
+
+			if err := helpers.NewAzureConfig(&az); err != nil {
+				fmt.Println("Error creating azure config file")
+				return
+			}
+		default:
+			fmt.Println("usage: dockvault configure <aws|azure>")
+		}
+
+	case "upload":
+		uploadCmd := flag.NewFlagSet("upload", flag.ExitOnError)
+		uploadCmd.Parse(args[1:])
+	default:
+		fmt.Printf("Unknown subcommand '%s'\n", args[1])
+	}
+
+	// c, err := helpers.NewConfig()
 	// if err != nil {
-	// 	log.Fatalln("S3 Failed", err)
+	// 	log.Fatalln("Config failed", err)
 	// }
-	// s3.Upload(storage.UploadParams{
-	// 	ImageId:  "alpine",
-	// 	BlobName: "alpine_s3",
-	// })
-	// err = s3.Load(storage.LoadParams{
-	// 	BlobName: "alpine_s3",
-	// })
+	// d, err := helpers.NewDocker()
 	// if err != nil {
-	// 	log.Fatal("Failed to download ", err)
+	// 	log.Fatal("Failed to start Docker... is it running?")
 	// }
 
-	az, err := storage.NewAzureWithAD(c, d)
-	if err != nil {
-		log.Fatal("Failed to initialize azure ", err)
-	}
-	// if err := az.Upload(storage.UploadParams{
-	// 	ImageId:  "alpine",
-	// 	BlobName: "alpine_blob",
-	// }); err != nil {
-	// 	log.Fatal("Failed to upload ", err)
-	// }
-	// if err := az.List(); err != nil {
-	// 	log.Fatal("Failed to list ", err)
-	// }
-	if err := az.Load(storage.LoadParams{BlobName: "alpine_blob"}); err != nil {
-		log.Fatal("Failed to load ", err)
-	}
 }
