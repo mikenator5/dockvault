f9bc453f9de49549ca1fb5abc5dd5fd3b5cd9090
Mike Dunn <mikenator5@gmail.com>
2024-03-22 12:55:03 -0600

    Test azure refactor

--- main.go
+++ main.go
@@ -16,18 +16,35 @@ func main() {
 		log.Fatal("Failed to start Docker... is it running?")
 	}
 
-	s3, err := storage.NewS3(c, d)
-	if err != nil {
-		log.Fatalln("S3 Failed", err)
-	}
+	// s3, err := storage.NewS3(c, d)
+	// if err != nil {
+	// 	log.Fatalln("S3 Failed", err)
+	// }
 	// s3.Upload(storage.UploadParams{
 	// 	ImageId:  "alpine",
 	// 	BlobName: "alpine_s3",
 	// })
-	err = s3.Load(storage.LoadParams{
-		BlobName: "alpine_s3",
-	})
+	// err = s3.Load(storage.LoadParams{
+	// 	BlobName: "alpine_s3",
+	// })
+	// if err != nil {
+	// 	log.Fatal("Failed to download ", err)
+	// }
+
+	az, err := storage.NewAzureWithAD(c, d)
 	if err != nil {
-		log.Fatal("Failed to download ", err)
+		log.Fatal("Failed to initialize azure ", err)
+	}
+	// if err := az.Upload(storage.UploadParams{
+	// 	ImageId:  "alpine",
+	// 	BlobName: "alpine_blob",
+	// }); err != nil {
+	// 	log.Fatal("Failed to upload ", err)
+	// }
+	// if err := az.List(); err != nil {
+	// 	log.Fatal("Failed to list ", err)
+	// }
+	if err := az.Load(storage.LoadParams{BlobName: "alpine_blob"}); err != nil {
+		log.Fatal("Failed to load ", err)
 	}
 }
---
+++ storage/azure.go
@@ -0,0 +1,105 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"dockVault/helpers"
+	"dockVault/internal/pkg/output"
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+)
+
+type Azure struct {
+	cfg        helpers.Config
+	d          helpers.Docker
+	blobClient *azblob.Client
+}
+
+func NewAzureWithAD(cfg helpers.Config, d helpers.Docker) (Azure, error) {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return Azure{}, err
+	}
+
+	client, err := azblob.NewClient(cfg.Azure.Account, cred, nil)
+	if err != nil {
+		return Azure{}, err
+	}
+
+	return Azure{cfg: cfg, d: d, blobClient: client}, nil
+}
+
+func (az *Azure) Upload(params UploadParams) error {
+	fmt.Printf("Uploading %s\n", params.ImageId)
+	compressedImg, err := az.d.SaveImageInMemory(params.ImageId)
+	if err != nil {
+		return err
+	}
+
+	if len(params.BlobName) == 0 {
+		params.BlobName = params.ImageId
+	}
+
+	size := int64(compressedImg.Len())
+	_, err = az.blobClient.UploadBuffer(context.TODO(), az.cfg.Azure.Container, params.BlobName, compressedImg.Bytes(), &azblob.UploadBufferOptions{
+		BlockSize:   int64(1024),
+		Concurrency: uint16(3),
+		Progress: func(bytesTransferred int64) {
+			percentage := int((float64(bytesTransferred) / float64(size)) * 100)
+			output.PrintProgressBar(percentage)
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\nSuccessfully saved %s\n", params.ImageId)
+	return nil
+}
+
+func (az *Azure) List() error {
+	pager := az.blobClient.NewListBlobsFlatPager(az.cfg.Azure.Container, nil)
+	blobCount := 0
+	for pager.More() {
+		page, err := pager.NextPage(context.TODO())
+		if err != nil {
+			return err
+		}
+
+		for _, blob := range page.Segment.BlobItems {
+			fmt.Println(*blob.Name)
+			blobCount++
+		}
+	}
+
+	if blobCount <= 0 {
+		fmt.Printf("No blobs found in container \"%s\"\n", az.cfg.Azure.Container)
+	}
+
+	return nil
+}
+
+func (az *Azure) Load(params LoadParams) error {
+	fmt.Printf("Downloading %s\n", params.BlobName)
+	get, err := az.blobClient.DownloadStream(context.TODO(), az.cfg.Azure.Container, params.BlobName, nil)
+	if err != nil {
+		return err
+	}
+
+	data := bytes.Buffer{}
+	retryReader := get.NewRetryReader(context.TODO(), nil)
+	_, err = data.ReadFrom(retryReader)
+	if err != nil {
+		return err
+	}
+
+	err = az.d.LoadImageFromBuffer(&data)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Successfully loaded %s\n", params.BlobName)
+	return nil
+}
